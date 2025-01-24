f176cec557a6c5ba3aba0d4f4c67f9f507bfc4a6
Sakura286 <sakura286@outlook.com>
2025-01-24 10:41:37 +0800

    add missing target

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-ninja -j$(nproc) -C out/Release-riscv64 chrome  |& tee build-$(date +'%m%d-%H%M%S').log
+ninja -j$(nproc) -C out/Release-riscv64 chrome chrome_sandbox content_shell chromedriver |& tee build-$(date +'%m%d-%H%M%S').log
